bfef95a33be3d39868cb4d0ce6bc80ec0e7daa8c
kapil <khatrikapil987@gmail.com>
2023-03-19 13:23:34 +0530

    cart added

--- client/src/Stores/cartStore.js
+++ client/src/Stores/cartStore.js
@@ -4,9 +4,9 @@ import { createJSONStorage, persist } from "zustand/middleware";
 const cartStore = create(persist((set,get)=>({
     cart:localStorage.getItem("cart")?localStorage.getItem("cart"):[],
     addToCart:async(product,quan)=>{
-        console.log("wor");
+        // console.log("wor");
         const cart = get().cart
-        console.log(cart);
+        // console.log(cart);
         const itemExist =await cart.find((i)=>(i.product._id===product._id))
         if(itemExist){
             return cart
@@ -14,6 +14,13 @@ const cartStore = create(persist((set,get)=>({
         else{
             set({cart:[...cart,{product,quantity:quan}]})
         }
+    },
+    setQuan:(prod,quan)=>{
+        const cart = get().cart
+        const index = cart.findIndex((i)=>(i.product._id===prod._id))
+        console.log(quan,"ok")
+        cart[index].quantity = quan
+        set({cart})
     }
 }),{name:"cart",storage:createJSONStorage(()=>localStorage)}))
 
--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -1,28 +1,50 @@
-import React, { useState } from "react";
-import { Button, Container, Grid, IconButton, Input, Stack, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import {
+  Button,
+  Container,
+  Grid,
+  IconButton,
+  Input,
+  Stack,
+  Typography,
+} from "@mui/material";
 import cartStore from "../Stores/cartStore";
 import AddIcon from "@mui/icons-material/Add";
 import RemoveIcon from "@mui/icons-material/Remove";
+import { Box } from "@mui/system";
 export default function Cart() {
   const cartItems = cartStore((state) => state.cart);
-  const [quan,setQuan] = useState(1)
-//   const incQuan = ()=>{
-//     if(productDetails.product.stock>quan){
-//       setQuan(quan+1)
-//     }
-//     else{return quan}
-//   }
-//   const decQuan = ()=>{
-//     if(quan>1){
-//       setQuan(quan-1)
-//     }
-//     else{return quan}
-//   }
+  const [price, setPrice] = useState(0);
+
+  const setQuantity = cartStore((state) => state.setQuan);
+  const [quan, setQuan] = useState(null);
+
+  const incQuan = async (prod, quantity) => {
+    if (prod.stock > quantity) {
+      setQuantity(prod, quantity + 1);
+    } else {
+      return setQuantity(quantity);
+    }
+  };
+  const decQuan = (prod, quantity) => {
+    if (quantity > 1) {
+      setQuantity(prod, quantity - 1);
+    } else {
+      return setQuantity(quantity);
+    }
+  };
+  useEffect(() => {
+    let total = 0;
+    cartItems.forEach(
+      (item) => (total = total + item.product.price * item.quantity)
+    );
+    setPrice(total);
+  }, [cartItems]);
   return (
     <>
-      <Container maxWidth="lg" sx={{ height: "80vh", width: "100vw" }}>
+      <Box  sx={{ background:"#EAEDED", width: "98.5vw", position:"relative",overflow:"hidden"}}>
         <Grid container>
-          <Grid item lg={8} md={8} sm={12}>
+          <Grid item lg={8} md={8} sm={12} sx={{padding:'1rem',width:"60%",margin:"2rem",background:"#fff",border:"2px solid gray"}}>
             <Stack
               direction={"row"}
               alignItems="center"
@@ -30,16 +52,21 @@ export default function Cart() {
               className="title"
               style={{
                 borderBottom: "2px solid gray",
-                paddingBottom: ".5rem",
-                margin: "2rem 0",
+                paddingBottom: ".5rem"
               }}
             >
               <Typography variant="h3">Shopping Cart</Typography>
-              <Typography variant="h6">Quantity</Typography>
+              <Typography variant="h6" sx={{ paddingTop: "3rem" }}>
+                Price
+              </Typography>
             </Stack>
-            <Stack justifyContent="space-around" spacing={3}>
+            <Stack justifyContent="space-around" spacing={3} sx={{marginTop:'2rem'}}>
               {cartItems.map((item) => (
-                <Stack direction={"row"} sx={{ height: "10rem" }}>
+                <Stack
+                  direction={"row"}
+                  sx={{ height: "10rem" }}
+                  key={item.product._id}
+                >
                   <img
                     src={item.product.images[0].url}
                     style={{
@@ -48,43 +75,93 @@ export default function Cart() {
                       border: "2px solid grey",
                     }}
                   />
-                  <div className="itemContent" style={{ paddingLeft: "2rem" }}>
+                  <div
+                    className="itemContent"
+                    style={{
+                      paddingLeft: "2rem",
+                      display: "flex",
+                      flexDirection: "row",
+                      justifyContent: "space-between",
+                      width: "100%",
+                    }}
+                  >
                     <div>
                       <Typography variant="h5">{item.product.name}</Typography>
-                      <Typography variant="subtitle1" sx={{ margin: "1rem 0" }}>
-                        ₹{item.product.price}
-                      </Typography>
+                      <div style={{ margin: "1.9rem 0" }}>
+                        <IconButton
+                          size="small"
+                          sx={{
+                            border: "2px solid gray",
+                            borderRadius: ".4rem",
+                            height: "2.2rem",
+                          }}
+                          onClick={() => decQuan(item.product, item.quantity)}
+                        >
+                          <RemoveIcon />
+                        </IconButton>
+                        <Input
+                          sx={{
+                            border: "2px solid gray",
+                            borderRadius: ".4rem",
+                          }}
+                          type="number"
+                          size="sm"
+                          value={item.quantity}
+                          slotProps={{
+                            input: {
+                              min: 1,
+                              max: item.product.stock,
+                            },
+                          }}
+                        />
+                        <IconButton
+                          size="small"
+                          sx={{
+                            border: "2px solid gray",
+                            borderRadius: ".4rem",
+                            height: "2.2rem",
+                          }}
+                          onClick={() => incQuan(item.product, item.quantity)}
+                        >
+                          <AddIcon />
+                        </IconButton>
+                      </div>
                       <Button variant="outlined" size="small">
                         Remove
                       </Button>
                     </div>
-                    <div className="quantity">
-                      <IconButton size="small" onClick={''}>
-                        <RemoveIcon />
-                      </IconButton>
-                      <Input
-                        type="number"
-                        size="sm"
-                        value={quan}
-                        slotProps={{
-                          input: {
-                            min: 1,
-                            max: item.product.stock,
-                          },
-                        }}
-                      />
-                      <IconButton size="small" onClick={'incQuan'}>
-                        <AddIcon />
-                      </IconButton>
+                    <div className="price">
+                      <Typography variant="subtitle1">
+                        ₹{(quan ? quan : item.quantity) * item.product.price}
+                      </Typography>
                     </div>
                   </div>
                 </Stack>
               ))}
             </Stack>
+            <Box
+              sx={{
+                borderTop: "2px solid grey",
+                width: "100%",
+                display: "flex",
+                justifyContent: "flex-end",
+                margin: "2rem 0 0 0",
+              }}
+            >
+              <Typography variant="h6">Subtotal: ₹{price}</Typography>
+            </Box>
           </Grid>
-          <Grid item lg={4} md={4} sm={12}></Grid>
+          <Grid item lg={3.2} md={3.2} sm={12}   sx={{ marginTop:'2rem' }} >
+          <Box
+            sx={{ width: "100%", height: "10rem", border: "2px solid gray",padding:"1rem",display:"flex",flexDirection:"column",alignItems:"center" ,background:"#fff"}}
+          >
+            <Typography variant="h5">Subtotal: ₹{price}</Typography>
+            <Button variant="outlined" sx={{width:"80%",marginTop:"2rem"}}>Proceed To Check Out</Button>
+          </Box>
+          </Grid>
+          
         </Grid>
-      </Container>
+      </Box>
     </>
   );
 }
