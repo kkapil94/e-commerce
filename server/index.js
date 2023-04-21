adf27f1f8c792f34852b8edbeb21f47db91af6a3
kapil khatri <107659798+kkapil94@users.noreply.github.com>
2023-04-21 07:37:20 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const app = express()
 app.use(cookieParser())
 app.use(cors({
     credentials: true,
-    origin:"https://e-commerce-kkapil94",
+    origin:"https://e-commerce-kkapil94.vercel.app",
     optionsSuccessStatus: 200,
   }));
 app.use(bodyParser.urlencoded({extended:true}))
