2afcc854f633c42ab6eb053f62f805ba46bbc9e6
kapil khatri <107659798+kkapil94@users.noreply.github.com>
2023-04-21 09:43:40 +0530

    Update webToken.js

--- server/utils/webToken.js
+++ server/utils/webToken.js
@@ -4,9 +4,7 @@ export const Token = (user,statusCode,res)=>{
     const token = jwt.sign({id:user._id},"SecretKey")
     //option for cookie
     const options = {
-        expires:new Date(
-            Date.now() + 5*24*60*60*1000
-        ),
+        maxAge: 86400000,
         httpOnly:true,
         sameSite:"None",
         secure:true,
