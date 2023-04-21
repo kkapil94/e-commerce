3974699065b853091ecfafe3db58fe801c723d4c
kapil khatri <107659798+kkapil94@users.noreply.github.com>
2023-04-21 08:41:37 +0530

    Update webToken.js

--- server/utils/webToken.js
+++ server/utils/webToken.js
@@ -7,7 +7,7 @@ export const Token = (user,statusCode,res)=>{
         expires:new Date(
             Date.now() + 5*24*60*60*1000
         ),
-        httpOnly:false,
+        httpOnly:true,
         secure:true,
         sameSite: 'none'
     }
