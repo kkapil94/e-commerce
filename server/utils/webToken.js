44ec71d9cf174e6af636a85f8a440a915e13cff6
kapil khatri <107659798+kkapil94@users.noreply.github.com>
2023-04-21 08:05:16 +0530

    Update webToken.js

--- server/utils/webToken.js
+++ server/utils/webToken.js
@@ -7,8 +7,7 @@ export const Token = (user,statusCode,res)=>{
         expires:new Date(
             Date.now() + 5*24*60*60*1000
         ),
-        httpOnly:true,
-        secure:true
+        httpOnly:false
     }
     res.status(statusCode).cookie("token",token,options).json({
         success:true,
