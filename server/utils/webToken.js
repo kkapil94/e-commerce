982f21c2ca8e7b0a2385775ca12bdd58c793f99e
kapil khatri <107659798+kkapil94@users.noreply.github.com>
2023-04-21 09:46:35 +0530

    Update webToken.js

--- server/utils/webToken.js
+++ server/utils/webToken.js
@@ -4,12 +4,10 @@ export const Token = (user,statusCode,res)=>{
     const token = jwt.sign({id:user._id},"SecretKey")
     //option for cookie
     const options = {
-        maxAge: 86400000,
-        httpOnly:true,
-        sameSite:"None",
-        secure:true,
-         path: '/',
-         domain: 'https://e-commerce-kkapil94.vercel.app'
+        expires:new Date(
+            Date.now() + 5*24*60*60*1000
+        ),
+        httpOnly:true
     }
     res.status(statusCode).cookie("token",token,options).json({
         success:true,
